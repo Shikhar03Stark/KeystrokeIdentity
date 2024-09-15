379134073e5355576fbeac91ee3e56504c7fb5c0
Shikhar03Stark <hv.harshit321@gmail.com>
2024-09-15 20:52:55 +0530

    fix: handle on_press

--- PoC/log_keystroke.py
+++ PoC/log_keystroke.py
@@ -2,32 +2,40 @@ import keyboard
 import time
 import csv
 
-def map_special_keys(key):
+def map_to_ascii(key):
     mapping = {
-        "space": "@",
-        "shift": "$",
-        "'": "#",
+        "shift": 16,
+        'backspace': 8,
+        'space': 32,
     }
     
-    return mapping[key] if key in mapping.keys() else key
+    return mapping[key] if key in mapping else ord(key.upper())
 
 ctx = {
-    "file_name": "keystrokes_3.csv",
+    "file_name": "keystrokes_5.csv",
     "buffer": [],
-    "allowed_keys": [ chr( ord('A') + i) for i in range(26)] + [ chr( ord('a') + i) for i in range(26)] + ["space", "shift", "'"] ,
-    "map_special_keys_fn": map_special_keys,
+    "allowed_keys": [ chr( ord('A') + i) for i in range(26)] + [ chr( ord('a') + i) for i in range(26)] + ["?", "space", "shift", "'", ",", ".", "backspace", "\""],
+    "map_to_ascii_fn": map_to_ascii,
 }
 
 def on_keystroke_callback(context):
-    print(ctx)
+    print(context)
+    last_down = []
     def on_keystroke(event):
         print(event.name)
         if event.name not in context["allowed_keys"]:
             return
-        event.name = context["map_special_keys_fn"](event.name)
+        event.name = context["map_to_ascii_fn"](event.name)
         if event.event_type == keyboard.KEY_DOWN:
+            if len(last_down) > 0 and last_down[0] == event.name:
+                return
+            if len(last_down) == 1:
+                last_down.pop()
+            last_down.append(event.name)
             on_key_down(event, context)
         elif event.event_type == keyboard.KEY_UP:
+            if len(last_down) == 1 and last_down[0] == event.name:
+                last_down.pop()
             on_key_up(event, context)
     
     return on_keystroke
